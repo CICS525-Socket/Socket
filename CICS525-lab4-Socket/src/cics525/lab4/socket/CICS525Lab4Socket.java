b8cc4c29b87dee171c153784248b9c48f85cfdc6
ali <alikn@ece.ubc.ca>
2014-06-23 15:32:01 -0700

    Title fixed.

---
+++ CICS525-lab4-Socket/src/cics525/lab4/socket/CICS525Lab4Socket.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cics525.lab4.socket;
+
+/**
+ *
+ * @author Ali
+ */
+public class CICS525Lab4Socket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Just a test
+    }
+    
+}
