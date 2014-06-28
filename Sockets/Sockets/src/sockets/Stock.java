c15a820a59f3a9ce958184579356be85d4a0a665
twizzyyanki <uiagu@mss.icics.ubc.ca>
2014-06-28 13:21:05 -0700

    Update Stock.java

--- Sockets/Sockets/src/sockets/Stock.java
+++ Sockets/Sockets/src/sockets/Stock.java
@@ -8,10 +8,56 @@ package sockets;
 
 /**
  *
- * @author Ali
+ * @author Ali k
  */
 public class Stock {
-    String tickerName;
-    float price;
-    int numberAvailable;
+	private double price;
+	private int no;
+	private String tickername;
+	
+	/* this constructor would be removed afterwards. It is just for test
+	 * 
+	 */
+	public Stock(String tickername, int no, double price){
+		this.tickername = tickername;
+		this.price = price;
+		this.no = no;
+	}
+	
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	/**
+	 * @return the no
+	 */
+	public int getNo() {
+		return no;
+	}
+	/**
+	 * @param no the no to set
+	 */
+	public void setNo(int no) {
+		this.no = no;
+	}
+	/**
+	 * @return the tickername
+	 */
+	public String getTickername() {
+		return tickername;
+	}
+	/**
+	 * @param tickername the tickername to set
+	 */
+	public void setTickername(String tickername) {
+		this.tickername = tickername;
+	}    
 }
