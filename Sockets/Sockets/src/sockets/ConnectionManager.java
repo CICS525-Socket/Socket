31679ed160dcbb99746b120545437df0e1c12dcb
ali <alikn@ece.ubc.ca>
2014-06-28 14:30:05 -0700

    server updated

--- Sockets/Sockets/src/sockets/ConnectionManager.java
+++ Sockets/Sockets/src/sockets/ConnectionManager.java
@@ -8,6 +8,7 @@ package sockets;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -17,19 +18,48 @@ import java.util.Scanner;
  */
 public class ConnectionManager implements Runnable{
     
-    private Socket socket;
+    private final Socket socket;
     private Scanner inStream;
     private OutputStream outStream;
-    
+    private PrintWriter out;
     
     public ConnectionManager(Socket socket) throws IOException{
         this.socket = socket;
         inStream = new Scanner(socket.getInputStream());
         outStream = socket.getOutputStream();
+        out = new PrintWriter(outStream);
     }
 
     public void run() {
        System.out.println("run method running.");
+       String userCommand = this.readInputStream();
+       System.out.println("user command: " + userCommand);
+       this.sendToUser(userCommand);
+       this.closeConnection();
+    }
+
+    private String readInputStream() {
+        if(inStream.hasNextLine()) {
+            String line = inStream.nextLine();
+            return line;
+        }
+        return null;
+    }
+
+    private void sendToUser(String userCommand) {
+        out.print(userCommand);
+        out.flush();
+    }
+
+    private void closeConnection() {
+        try{
+        inStream.close();
+        outStream.close();
+        out.close();
+        socket.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
     
     
